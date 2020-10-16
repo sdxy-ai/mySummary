848ac98ffe6f1d7e67ae75f91fef9fee170d3338
sdxy-ai <280411879@qq.com>
2020-10-16 18:18:52 +0800

    删除了Login

--- Login/my-server/routesHandle/register.js
+++
@@ -1,8 +0,0 @@
-const connection=require('../db/index');
-
-
-module.exports=async(ctx, next) => {
-  const sql='INSERT INTO user set?';
-  const { username,email,password }=ctx.request.body;
-  connection(sql,{username,email,password});
-}
